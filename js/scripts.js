abbe6e9eb9b6e50d0e39bd435f1b66e545d3a00d
Bartek <bartekma89@gmail.com>
2017-12-14 20:20:45 +0100

    task completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,50 @@
-function Car(model, manufacturer) {
-
-	this.model = model;
-	this.manufacturer = manufacturer;
-
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
 }
 
-Car.prototype.logModel = function () {
-	console.log("This car's model is called " + this.model + ".");
+Phone.prototype.printInfo = function() {
+	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price);
 };
 
-var volvo = new Car('XC60', 'Volvo');
+var huaweiP9 = new Phone('Huawei', 1500, 'black');
+var iphoneX = new Phone('Apple', 5000, 'silver');
+var samsungGalaxy8 = new Phone('Samsung', 3799, 'black');
+var sonyXperiaZ = new Phone('Sony', 3399, 'white');
 
-function Person(name, age) {
-	
-	this.name = name;
-	this.age = age || 30;
-	this.sayHello = function() {
-		console.log('Hi, my name is ' + this.name);
-	};
-}
+huaweiP9.printInfo();
+iphoneX.printInfo();
+samsungGalaxy8.printInfo();
+sonyXperiaZ.printInfo();
 
-var person1 = new Person('Jan', 35);
-var person2 = new Person('Adam', 32);
-var person3 = new Person('Ewa');
+iphoneX.category = 'premium';
+samsungGalaxy8.category = 'premium';
 
-person1.sayHello();
-person2.sayHello();
+console.log(iphoneX.category);
+
+Phone.prototype.canIAfford = function() {
+	if(this.price > 2500) {
+		console.log('You dont have enought money for ' + this.brand)
+	} else {
+		console.log('You can have great gift, buying phone from ' + this.brand);
+	}
+};
+
+huaweiP9.canIAfford();
+samsungGalaxy8.canIAfford();
+
+Phone.prototype.applyDiscount = function(discount) {
+	var newPrice = this.price - discount;
+	if(newPrice < 3000) {
+		console.log('You can buy your imagine phone from ' + this.brand + ', for new price: ' + newPrice );
+	} else {
+		console.log('Not this time. Currently brand ' + this.brand + ' is out of your reach');
+	}
+};
+
+
+console.log('Can you buy your imagine ?');
+iphoneX.applyDiscount(2000);
+samsungGalaxy8.applyDiscount(1588);
+huaweiP9.applyDiscount(120);
